77c3b36aad19a10afb2ce666c15ddd3d46d97eeb
Julia Prinz <2510859027@hochschule-burgenland.at>
2026-06-02 09:21:09 +0200

    Test Guest

--- src/main/java/at/fhburgenland/Main.java
+++ src/main/java/at/fhburgenland/Main.java
@@ -1,24 +1,21 @@
 package at.fhburgenland;
 
+import at.fhburgenland.entity.Guest;
 import jakarta.persistence.*;
 
 import java.util.List;
 
 public class Main {
-    private static EntityManagerFactory EMF = Persistence.createEntityManagerFactory("Gruppe3_HMS");
+    private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("Gruppe3_HMS");
 
     public static void main(String[] args) {
-
-        System.out.println("Test");
-        /* TO DO
-            -) Connect Database
-            -) Klasse zur Tabelle erstellen!
-            -) Create Methods for
-                -) addPerson
-                -) readPerson
-                -) readAllPersons --> Ausgabe ganze Tabelle
-                -) update Person
-                -) delete Person
-         */
+        EntityManager em = emf.createEntityManager();
+        List<Guest> guests = em.createQuery("select * from guest", Guest.class)
+                .getResultList();
+        for (Guest guest : guests) {
+            System.out.println(guest);
+        }
+        em.close();
+        emf.close();
     }
 }
