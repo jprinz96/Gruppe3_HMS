6b5039a7e9810a41a1c4802d6661cc6ca75c252d
Julia Prinz <2510859027@hochschule-burgenland.at>
2026-06-02 07:49:57 +0200

    Connection to the database created using Hibernate

--- src/main/java/at/fhburgenland/Main.java
+++ src/main/java/at/fhburgenland/Main.java
@@ -1,11 +1,14 @@
 package at.fhburgenland;
 
 import jakarta.persistence.*;
+
 import java.util.List;
 
 public class Main {
+    private static EntityManagerFactory EMF = Persistence.createEntityManagerFactory("Gruppe3_HMS");
 
     public static void main(String[] args) {
+
         System.out.println("Test");
         /* TO DO
             -) Connect Database
