48b016ca163f3794ff6bafe8c73f2bc9dcc8bc7e
Julia Prinz <2510859027@hochschule-burgenland.at>
2026-06-02 10:04:26 +0200

    Test DB
    Test: Display all data in the database

--- src/main/java/at/fhburgenland/Main.java
+++ src/main/java/at/fhburgenland/Main.java
@@ -6,16 +6,81 @@ import jakarta.persistence.*;
 import java.util.List;
 
 public class Main {
-    private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("Gruppe3_HMS");
 
     public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("Gruppe3_HMS");
         EntityManager em = emf.createEntityManager();
-        List<Guest> guests = em.createNativeQuery("select * from guest", Guest.class)
-                .getResultList();
-        for (Guest guest : guests) {
-            System.out.println(guest);
-        }
+
+        printZips(em);
+        printGuests(em);
+        printRooms(em);
+        printReservations(em);
+
         em.close();
         emf.close();
+    }
+
+    private static void printZips(EntityManager em) {
+        System.out.println("\n--- ZIP ---");
+
+        List<Zip> zips = em.createQuery("select z from Zip z", Zip.class)
+                .getResultList();
+
+        for (Zip zip : zips) {
+            System.out.println(zip.getZipcode() + " - " + zip.getCity());
+        }
+    }
+
+    private static void printGuests(EntityManager em) {
+        System.out.println("\n--- GUEST ---");
+
+        List<Guest> guests = em.createQuery("select g from Guest g", Guest.class)
+                .getResultList();
+
+        for (Guest guest : guests) {
+            System.out.println(
+                    guest.getGuestId() + " - " +
+                            guest.getFirstname() + " " +
+                            guest.getLastname() + " - " +
+                            guest.getEmail() + " - " +
+                            guest.getZip().getZipcode() + " " +
+                            guest.getZip().getCity()
+            );
+        }
+    }
+
+    private static void printRooms(EntityManager em) {
+        System.out.println("\n--- ROOM ---");
+
+        List<Room> rooms = em.createQuery("select r from Room r", Room.class)
+                .getResultList();
+
+        for (Room room : rooms) {
+            System.out.println(
+                    room.getRoomId() + " - Zimmer " +
+                            room.getRoomNumber() + " - " +
+                            room.getCategory() + " - " +
+                            room.getPricePerNightEur() + " EUR"
+            );
+        }
+    }
+
+    private static void printReservations(EntityManager em) {
+        System.out.println("\n--- RESERVATION ---");
+
+        List<Reservation> reservations = em.createQuery("select r from Reservation r", Reservation.class)
+                .getResultList();
+
+        for (Reservation reservation : reservations) {
+            System.out.println(
+                    reservation.getReservationId() + " - " +
+                            reservation.getGuest().getFirstname() + " " +
+                            reservation.getGuest().getLastname() + " - " +
+                            reservation.getCheckinDate() + " bis " +
+                            reservation.getCheckoutDate() + " - " +
+                            reservation.getStatus()
+            );
+        }
+
     }
 }
